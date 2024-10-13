3188d6ef8d792aab9e31a04ce73a38e84189ca84
Catalina Matheus <catalina.matheus@correo.unimet.edu.ve>
2024-10-13 18:20:39 -0400

    🐛feat y fix: ya se muestran los datos que estan en la BD y arreglado el controlador del parking lot

--- lib/controllers/parking_lot_controller.dart
+++ lib/controllers/parking_lot_controller.dart
@@ -8,11 +8,11 @@ class ParkingLotController {
     final plStream = _supabase.from('parkingLot').stream(primaryKey: ['id']);
     return plStream.map((maps) => maps
         .map((map) => ParkingLot(
-            id: map['id'],
-            name: map['name'],
-            location: map['location'],
-            totalParkingSpace: map['totalParkingSpace'],
-            cars: map['cars']))
+            id: map['id'].toString(),
+            name: map['name'].toString(),
+            location: map['location'].toString(),
+            totalParkingSpace: map['totalParkingSpace'] as int,
+            cars: map['cars'] as int))
         .toList());
   }
 }
