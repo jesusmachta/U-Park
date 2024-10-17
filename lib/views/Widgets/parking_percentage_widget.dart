d33620b546af7869d376416f62d4e30f9da20902
Catalina Matheus <catalina.matheus@correo.unimet.edu.ve>
2024-10-17 14:16:46 -0400

    💄Feat: agregado el loader de las estadisticas

--- lib/views/Widgets/parking_percentage_widget.dart
+++ lib/views/Widgets/parking_percentage_widget.dart
@@ -70,6 +70,8 @@ import 'parking_percentage_card.dart';
 // }
 
 
+import 'package:primera_app/controllers/parking_lot_controller.dart'; // Importar tu controlador
+
 class ParkingPercentageWidget extends StatefulWidget {
   const ParkingPercentageWidget({Key? key}) : super(key: key);
 
@@ -79,7 +81,7 @@ class ParkingPercentageWidget extends StatefulWidget {
 }
 
 class _ParkingPercentageWidgetState extends State<ParkingPercentageWidget> {
-  final ParkingLotController _controller = ParkingLotController();
+  final ParkingLotController controller = ParkingLotController();
   List<ParkingLot> parkingLots = [];
 
   @override
@@ -90,7 +92,7 @@ class _ParkingPercentageWidgetState extends State<ParkingPercentageWidget> {
 
   // Método para escuchar los datos en tiempo real
   void _listenToParkingData() {
-    _controller.getParkingLots().listen((lots) {
+    controller.getParkingLots().listen((lots) {
       setState(() {
         parkingLots = lots; // Actualizar la lista de estacionamientos
       });
@@ -99,15 +101,62 @@ class _ParkingPercentageWidgetState extends State<ParkingPercentageWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    double screenHeight = MediaQuery.of(context).size.height; 
+    return StreamBuilder<List<ParkingLot>>(
+      stream: controller.getParkingLots(),
+      builder: (context,snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+          final size = MediaQuery.of(context).size;
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    0, // Izquierda
+                    screenHeight * 0.3, // Arriba
+                    0, // Derecha
+                    0, // Abajo
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(
+                        strokeWidth: size.width * 0.02,
+                        valueColor: const AlwaysStoppedAnimation<Color>(
+                            Colors.orangeAccent),
+                      ),
+                      SizedBox(height: size.height * 0.02),
+                      Text(
+                        'Cargando estadísticas...',
+                        style: TextStyle(
+                          fontSize: size.width * 0.04,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        if(snapshot.hasError){
+          return Text('Error: ${snapshot.error}');
+        }
+        final parkingLots = snapshot.data ?? [];
+        return Column(
       children: parkingLots.map((lot) {
         return ParkingPercentageCard(
-          key: ValueKey(lot.id), // Asignar una clave única basada en el ID
+          key: ValueKey(lot.id), 
           parkingName: lot.name,
           currentCars: lot.cars,
           totalSpots: lot.totalParkingSpace,
         );
       }).toList(),
     );
+      }
+    );
+    
   }
 }
